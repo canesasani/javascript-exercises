94a8254260507db5d0125d8fbb0acb4df2dd5fab
Canes Asani <canesasani@gmail.com>
2025-12-10 20:47:06 +0300

    Finish 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+	if (num < 0) {
+		return "ERROR";
+	}
+	let finalWord = "";
+	for (let i = 0; i < num; i++) {
+		finalWord += str;
+	}
+	return finalWord;
 };
+
+repeatString("CAN", 5);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 06_repeatString/solution/repeatString-solution.js
+++ 06_repeatString/solution/repeatString-solution.js
@@ -1,10 +1,12 @@
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
-  }
-  return string;
+const repeatString = function (str, num) {
+	if (num < 0) {
+		return "ERROR";
+	}
+	let finalWord = "";
+	for (let i = 0; i < num; i++) {
+		finalWord += str;
+	}
+	return finalWord;
 };
 
 module.exports = repeatString;
