4714d7cf7d0832aa0b41f5846472940946236ec2
Canes Asani <canesasani@gmail.com>
2025-12-11 20:04:28 +0300

    Finish 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+	let celcius = ((num - 32) * 5) / 9;
+	let round = Math.round(celcius * 10) / 10;
+	return round;
 };
 
-const convertToFahrenheit = function() {
+// (°F − 32) × 5/9 = °C
+
+const convertToFahrenheit = function (num) {
+	let fahrenheit = (num * 9) / 5 + 32;
+	let round = Math.round(fahrenheit * 10) / 10;
+	return round;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
