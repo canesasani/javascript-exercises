059fb0f1dda1f790eaed8cc3a86f912b322d7e00
Canes Asani <canesasani@gmail.com>
2025-12-11 19:00:29 +0300

    Finish 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr, ...nums) {
+// 	return arr.filter((item) => item != nums);
+// };
+
+const removeFromArray = function (arr, ...nums) {
+	return arr.filter((item) => !nums.includes(item));
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
