31735501eb34b79a18ed5ee26dfa3bd7e4edfae2
Canes Asani <canesasani@gmail.com>
2025-12-11 19:53:59 +0300

    Finish 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+// 1- If year % 4 OR (year % 100 AND year % 400) return true, if not return false
 
+const leapYears = function (year) {
+	if ((year % 4 === 0 && !(year % 100 === 0)) || year % 400 === 0) {
+		return true;
+	} else {
+		return false;
+	}
 };
+
+// isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)
 
 // Do not edit below this line
 module.exports = leapYears;
